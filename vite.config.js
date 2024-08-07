807ce6fdb6b0e52ff4ce13b8af47f8ccca851cc6
Charbel Abboud <charbelabboud08@gmail.com>
2024-08-08 02:11:14 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Ensure that .git and other unnecessary files are not included in the build
+    rollupOptions: {
+      external: ['.git/*']
+    },
+    // Optionally specify what directories/files to exclude from the build
+    // Vite itself does not directly support a 'exclude' option, but this is a common approach
+  },
+});
