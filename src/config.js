78a866f08c0a8a79256afde88318fd0ca7e9aa6a
Charbel Abboud <charbelabboud08@gmail.com>
2024-08-08 02:00:32 +0300

    setup gh-pages

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-// replace below firebase config with your own
 
 const firebaseConfig = {
   apiKey: "AIzaSyDHu3Up3CygxJ62FPskGnwQlwe2bRGb5gI",
@@ -8,11 +7,6 @@ const firebaseConfig = {
   messagingSenderId: "657468426406",
   appId: "1:657468426406:web:ebe94dbcb1a86115948c67"
 };
-
-
-
-// craete a/c on themoviedb.org and replace access token below
-
 
 const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzE5ZGZjNmUyZTAzYzI1ZDg5ZTI5Mzc4YjBkYTk3MSIsIm5iZiI6MTcyMzA2MDk2MS42MTM1NzIsInN1YiI6IjY2YjNkMGUxNzc1MDVmN2ZlNDU0MmEyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zMjY2l4GAIcCl2m7ZSNiAsGVdBfZF8LLfIpiMP5-s9s.vPDW2QwNr9hIRdOvJx_x8hbHnDYZGHMtnZwfkqb3J8U";
 
